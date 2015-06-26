9a5b912c7c79343ea1cefad9ae697916bab44df7
Chris <domi981901@gmail.com>
2015-06-26 18:36:44 +0200

    ...

--- app/src/main/java/com/lfg/informatik/q11/quizzing4abi/test987.java
+++ app/src/main/java/com/lfg/informatik/q11/quizzing4abi/test987.java
@@ -3,15 +3,16 @@ package com.lfg.informatik.q11.quizzing4abi;
 /**
  * Created by dominik muhahaha on 26.06.2015.
  */
-public class test987 {
+public class test987
+{
     // much wow such class k?
     void test()
     {
 
     }
-    void test2(){
+
+    void test2()
+    {
 
     }
-
-
 }
