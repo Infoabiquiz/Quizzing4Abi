6f0b9f4cbe002ee382ccc25ecdf3cb7243c06096
dominik <domi98101@gmail.com>
2015-06-26 18:19:39 +0200

    test new class test987

--- app/src/main/java/com/lfg/informatik/q11/quizzing4abi/test987.java
+++ app/src/main/java/com/lfg/informatik/q11/quizzing4abi/test987.java
@@ -1,10 +1,10 @@
 package com.lfg.informatik.q11.quizzing4abi;
 
 /**
- * Created by dominik on 26.06.2015.
+ * Created by dominik muhahaha on 26.06.2015.
  */
 public class test987 {
-
+    // much wow such class
     return;
 
 }
