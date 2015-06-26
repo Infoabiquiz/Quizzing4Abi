cb85635a3bd49daa7dec6743f271c7514a5c6655
Chris <domi981901@gmail.com>
2015-06-26 18:08:36 +0200

    First test upload

---
+++ app/src/androidTest/java/com/lfg/informatik/q11/quizzing4abi/ApplicationTest.java
@@ -0,0 +1,15 @@
+package com.lfg.informatik.q11.quizzing4abi;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application>
+{
+    public ApplicationTest()
+    {
+        super(Application.class);
+    }
+}
