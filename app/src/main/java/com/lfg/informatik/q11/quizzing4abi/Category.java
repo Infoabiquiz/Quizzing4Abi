997ef5e066e67ae3303d9ba553f2b1069bcb9456
Adrian <domi981901@gmail.com>
2015-06-27 20:36:02 +0200

    Merge branch 'master' of C:\Users\AdriN\AndroidStudioProjects\Quizzing4Abi with conflicts.

--- app/src/main/java/com/lfg/informatik/q11/quizzing4abi/Category.java
+++ app/src/main/java/com/lfg/informatik/q11/quizzing4abi/Category.java
@@ -1,29 +1,38 @@
 package com.lfg.informatik.q11.quizzing4abi;
-
 import java.util.ArrayList;
 
 /**
  * Created by Adrian on 27.06.2015.
  */
-
-public class Category
-{
+public class Category {
     String questionCategory;
     ArrayList<Question> questions;
 
-    public Category (String questionCategory,ArrayList<Question> questions)
-    {
-        this.questionCategory = questionCategory;
-        this.questions = questions;
-    }
+public Category (String questionCategory,ArrayList<Question> questions )
+{
+    this.questionCategory=questionCategory;
+    this.questions=questions;
+}
+
+    /**
+     *
+     * @return
+     */
 
     String getCategoryName()
     {
         return questionCategory;
     }
 
+    /**
+     *Returns the questions
+     * @return list of questions
+     */
+
     ArrayList<Question> getQuestions()
     {
         return questions;
     }
+
+
 }
