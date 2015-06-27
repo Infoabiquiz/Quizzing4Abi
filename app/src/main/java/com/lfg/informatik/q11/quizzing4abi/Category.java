3e127fef6b569d5f11ee221649b56baab3ef806c
Adrian <domi981901@gmail.com>
2015-06-27 20:30:21 +0200

    Edit class Category

---
+++ app/src/main/java/com/lfg/informatik/q11/quizzing4abi/Category.java
@@ -0,0 +1,29 @@
+package com.lfg.informatik.q11.quizzing4abi;
+import java.util.ArrayList;
+
+/**
+ * Created by Adrian on 27.06.2015.
+ */
+public class Category {
+    String questionCategory;
+    ArrayList<Question> questions;
+
+public Category (String questionCategory,ArrayList<Question> questions )
+{
+    this.questionCategory=questionCategory;
+    this.questions=questions;
+}
+
+
+    String getCategoryName()
+    {
+        return questionCategory;
+    }
+
+    ArrayList<Question> getQuestions()
+    {
+        return questions;
+    }
+
+
+}
