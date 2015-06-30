d804cdb34db6d7ce3cfc6e7169be505b1a7eebb7
Aleksandar <domi98191@gmail.com>
2015-06-30 13:46:42 +0200

    Created MainMenuState class

---
+++ app/src/main/java/com/lfg/informatik/q11/quizzing4abi/MainMenuState.java
@@ -0,0 +1,22 @@
+package com.lfg.informatik.q11.quizzing4abi;
+
+import android.view.View;
+
+/**
+ * Created by Aleksandar on 30.06.2015.
+ */
+
+public class MainMenuState extends AppState
+
+{
+    MainMenuState(Application application)
+    {
+        super(application);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        super.onClick(view);
+    }
+}
