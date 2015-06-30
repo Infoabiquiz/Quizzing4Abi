4f2ee1a4d597a593a3c7acf7b0013ca5429877e2
Chris <domi981901@gmail.com>
2015-06-30 13:16:01 +0200

    AppState class

--- app/src/main/java/com/lfg/informatik/q11/quizzing4abi/AppState.java
+++ app/src/main/java/com/lfg/informatik/q11/quizzing4abi/AppState.java
@@ -1,8 +1,10 @@
 package com.lfg.informatik.q11.quizzing4abi;
 
+import android.view.View;
+
 /**
  * Created by Chris on 30.06.2015.
- *
+ * Base class for all app states. The State pattern is applied.
  */
 
 public class AppState
@@ -12,8 +14,10 @@ public class AppState
     public AppState(Application application)
     {
         this.application = application;
-
     }
 
+    public void onClick(View view)
+    {
 
+    }
 }
