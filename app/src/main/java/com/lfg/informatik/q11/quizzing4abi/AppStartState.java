f31a16f9c57a7f4edc80008d915d988ca04cb4b9
Chris <domi981901@gmail.com>
2015-06-30 20:02:13 +0200

    Created AppStartState class

---
+++ app/src/main/java/com/lfg/informatik/q11/quizzing4abi/AppStartState.java
@@ -0,0 +1,32 @@
+package com.lfg.informatik.q11.quizzing4abi;
+
+import android.view.View;
+
+/**
+ * Created by Chris on 30.06.2015.
+ * The first state of the app after starting it.
+ */
+
+public class AppStartState extends AppState
+{
+    /**
+     * Constructor.
+     * @param application a valid Application
+     */
+    public AppStartState(Application application)
+    {
+        super(application);
+
+        // TODO: application.setLayout(R.layout.app_start);
+    }
+
+    /**
+     * Handles user click events.
+     * @param view the click source (e.g. button)
+     */
+    @Override
+    public void onClick(View view)
+    {
+        // TODO: Add functionality
+    }
+}
