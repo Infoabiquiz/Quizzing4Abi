1d6f8aaba31bb70268f43609b6757354ac30fd39
dominik <domi98101@gmail.com>
2015-07-02 12:41:47 +0200

    correction application.java

---
+++ app/src/main/java/com/lfg/informatik/q11/quizzing4abi/app_state_classes/SettingsState.java
@@ -0,0 +1,35 @@
+package com.lfg.informatik.q11.quizzing4abi.app_state_classes;
+
+import android.view.View;
+
+import com.lfg.informatik.q11.quizzing4abi.Application;
+
+/**
+ * Created by Dominik on 02.07.2015.
+ * State of the Settings.
+ */
+
+public class SettingsState extends AppState
+{
+    /**
+     * Constructor.
+     * @param application a valid Application
+     */
+    public SettingsState(Application application)
+    {
+        super(application);
+
+
+       // TODO: application.setLayout(R.layout.settings);
+    }
+
+    /**
+     * Handles user click events.
+     * @param view the click source (e.g. button)
+     */
+    @Override
+    public void onClick(View view)
+    {
+        // TODO: Add functionality
+    }
+}
