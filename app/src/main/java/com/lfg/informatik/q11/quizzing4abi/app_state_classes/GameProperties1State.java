fb78753c1c695c7d25edfb85125e9afcefe0fa16
Chris <domi981901@gmail.com>
2015-07-03 18:30:23 +0200

    Edited the GameProperties classes

--- app/src/main/java/com/lfg/informatik/q11/quizzing4abi/app_state_classes/GameProperties1State.java
+++ app/src/main/java/com/lfg/informatik/q11/quizzing4abi/app_state_classes/GameProperties1State.java
@@ -3,23 +3,25 @@ package com.lfg.informatik.q11.quizzing4abi.app_state_classes;
 import android.view.View;
 
 import com.lfg.informatik.q11.quizzing4abi.Application;
+import com.lfg.informatik.q11.quizzing4abi.R;
 
 /**
  * Created by Chris on 01.07.2015.
- * The first state if a new Game is started. It shows the game creation options.
+ * The first state if a new Game is started. It shows the available Categories,
+ * from which the user chooses from.
  */
 
-public class GamePropertiesState extends GameState
+public class GameProperties1State extends GameState
 {
     /**
      * Constructor.
      * @param application a valid Application
      */
-    public GamePropertiesState(Application application)
+    public GameProperties1State(Application application)
     {
         super(application);
 
-        // TODO: application.setLayout(R.layout.game_properties);
+        application.setLayout(R.layout.game_properties1);
     }
 
     /**
